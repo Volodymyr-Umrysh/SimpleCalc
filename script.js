3c6b7b2c2a2440e61dc8337d411e40098dff50ef
Volodymyr Umrysh <umrysh.v@gmail.com>
2020-02-13 23:56:48 +0200

    init

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 let obj = {
     '1':{
         inputs:document.querySelectorAll('div[data-row="1"] input'),
-        resultContainer:document.querySelector('.result1')
+        resultContainer:document.querySelector('.result1'),
+        inputContainer:document.querySelector('.inputNumbers1')
     },
     '2':{
         inputs:document.querySelectorAll('div[data-row="2"] input'),
-        resultContainer:document.querySelector('.result2')
+        resultContainer:document.querySelector('.result2'),
+        inputContainer:document.querySelector('.inputNumbers2')
     },
     '3':{
         inputs:document.querySelectorAll('div[data-row="3"] input'),
-        resultContainer:document.querySelector('.result3')
+        resultContainer:document.querySelector('.result3'),
+        inputContainer:document.querySelector('.inputNumbers3')
     }
 }
+document.querySelector('.checkBoxs').addEventListener('change', function (e) {
+let checkboxRow = e.target.dataset.row
+let isChecked = e.target.checked
+if(isChecked){
+    obj[checkboxRow].inputContainer.style.backgroundColor = '#28a745'
+}else{
+    obj[checkboxRow].inputContainer.style.backgroundColor = ''
+}
+
+    
+})
 
 let checkboxes = document.querySelectorAll('#blankCheckbox')
 
@@ -19,9 +33,9 @@ let checkboxes = document.querySelectorAll('#blankCheckbox')
 document.getElementById("result").onclick=function () {
     let sumAction = document.querySelector('#radio-sum')
     let checkedRows =  Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.dataset.row)
-    console.log(checkedRows)
+  
     checkedRows.forEach(row => {
-        let result = 0
+        let result = 0 
         obj[row].inputs.forEach(input => {
             let value = +input.value
             if(sumAction.checked && value){
